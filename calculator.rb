dabdf09fa75b99317bb88ae4174057a32900eafd
Ricky dildy <rdildy92@gmail.com>
2019-09-19 04:31:06 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,12 +1,12 @@
 # Add your variables here
 first_number = "1"
-input 
+ 
    expect (first_number).to be_an(Integer)
   
 end
 
 second_number = "2"
-input 
+
 expect(second_number).to be_an(Integer)
 expect(second_number).not_to equal (0)
 end
