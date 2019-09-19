40b984787ce095025cb80161e7e64b9ae5377298
Ricky dildy <rdildy92@gmail.com>
2019-09-19 04:36:07 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -3,10 +3,9 @@ first_number = "1"
  
    expect (first_number).to be_an(Integer)
   
-end
+
 
 second_number = "2"
 
 expect(second_number).to be_an(Integer)
 expect(second_number).not_to equal (0)
-end
