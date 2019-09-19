e9c828f1130f582798bc532d38f51ede25fb70e2
Ricky dildy <rdildy92@gmail.com>
2019-09-19 04:30:14 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -2,10 +2,11 @@
 first_number = "1"
 input 
    expect (first_number).to be_an(Integer)
-   expect (first_number)
+  
 end
 
 second_number = "2"
+input 
 expect(second_number).to be_an(Integer)
 expect(second_number).not_to equal (0)
 end
