225a749aa3856142a403f8dc3537d4c99fd8a28d
Ricky dildy <rdildy92@gmail.com>
2019-09-19 04:45:48 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,14 +1,17 @@
 # Add your variables here
 first_number = "2"
- 
+ do
    expect (first_number).to be_an(Integer)
-  END
+  end
 
 
 second_number = "4"
-
+do 
 expect(second_number).to be_an(Integer)
 expect(second_number).not_to equal (0)
-END 
+end
 
-first_number + second_number
+sum = first_number + second_number
+do 
+expect (sum).to equal (first_number+second_number)
+end 
