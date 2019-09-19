840ca0e8f3441aedcb9bcf3dc172e13e7eb3288f
Ricky dildy <rdildy92@gmail.com>
2019-09-19 04:39:03 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,11 +1,11 @@
 # Add your variables here
-first_number = "1"
+first_number = "2"
  
    expect (first_number).to be_an(Integer)
   END
 
 
-second_number = "2"
+second_number = "4"
 
 expect(second_number).to be_an(Integer)
 expect(second_number).not_to equal (0)
